b18616d8c0f4556dc65adf0ba9758084340cd19a
David Davis <xantus@cpan.org>
2008-01-06 21:09:24 +0000

    Import of XANTUS/POE-Component-Client-Asterisk-Manager-0.08 from CPAN.
    gitpan-cpan-distribution: POE-Component-Client-Asterisk-Manager
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         XANTUS/POE-Component-Client-Asterisk-Manager-0.08.tar.gz
    gitpan-cpan-author:       XANTUS
    gitpan-cpan-maturity:     released

--- Manager.pm
+++ Manager.pm
@@ -12,7 +12,7 @@ package POE::Component::Client::Asterisk::Manager;
 use strict;
 use warnings;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use Carp qw(croak);
 use POE qw( Component::Client::TCP );
